4616958077bad3a11721fdad8632c2973bb56d2d
yixuan <wangyixuan98@hotmail.com>
2018-01-27 14:53:46 -0500

    bootstrap index

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 
-
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
