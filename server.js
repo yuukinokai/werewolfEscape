dd32785caa5b0297fa7474c7b3ac44f5233fcfa4
yixuan <wangyixuan98@hotmail.com>
2018-01-27 12:22:22 -0500

    initial nodejs setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.render('index');
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+//parse form field from html body
+/*app.post('/', function (req, res) {
+  res.render('index');
+  console.log(req.body.city);
+})*/
