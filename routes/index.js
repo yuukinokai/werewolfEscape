2692076ce23f848fd1612c18cbad801ceb035762
Christiana Chen <cchen@chen-g96y.wireless.concordia.ca>
2018-01-27 22:05:25 -0500

    new maps

--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,16 @@ app.get('/game', function(req, res, next) {
   res.render('game');
 });
 
-app.get('/map', function(req, res, next) {
-  res.render('map');
+app.get('/map-1', function(req, res, next) {
+  res.render('map-1');
+});
+
+app.get('/map-2', function(req, res, next) {
+  res.render('map-2');
+});
+
+app.get('/map-3', function(req, res, next) {
+  res.render('map-3');
 });
 
 app.get('/create', function(req, res, next) {
