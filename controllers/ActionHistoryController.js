887c054114cf5339ccf05cfe68ada4f13dccc851
yixuan <wangyixuan98@hotmail.com>
2018-01-28 10:40:38 -0500

    index toggle

---
+++ controllers/ActionHistoryController.js
@@ -0,0 +1,11 @@
+var GameSession = require('../models/gamesession.js');
+var UserSession = require('../models/usersession.js');
+var ActionHistory = require('../models/actionhistory.js');
+
+function addAction(action){
+	var key = action;
+	
+}
+
+function listActions(){
+}
